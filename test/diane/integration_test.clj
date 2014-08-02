f6f7a34b81ef08baac1726ea833006698ca6948e
Dan Fuchs <fuchsd@gmail.com>
2014-08-01 23:09:14 -0500

    Cleanup integration tests a bit

--- test/diane/integration_test.clj
+++ test/diane/integration_test.clj
@@ -13,19 +13,22 @@
        (do ~@body)
        (finally (server/stop)))))
 
+(defn get-events
+  "Subscribe to an event stream at the test server path and return a vector
+  with n events from the stream"
+  [n path]
+  (with-test-server
+    (let [[events state close] (client/subscribe (str "http://localhost:" test-server/port path) {})
+          n-results (async/take n events)
+          all-results (<!! (async/into [] n-results))]
+      (close)
+      all-results)))
+
 ;; One event
-(expect {:origin  "http://localhost:9890/one-event", :data  "woohoo!", :event  "message", :last-event-id  ""}
-       (with-test-server
-         (let [[events state close] (client/subscribe (str "http://localhost:" test-server/port "/one-event") {})
-               result (<!! events)]
-           (close)
-           result)))
+(expect [{:origin  "http://localhost:9890/one-event", :data  "woohoo!", :event  "message", :last-event-id  ""}]
+        (get-events 1 "/one-event"))
 
 ;; Multiple events
 (expect  [{:origin  "http://localhost:9890/multiple-events", :data  "woohoo!", :event  "message", :last-event-id  ""}
-         {:origin  "http://localhost:9890/multiple-events", :data  "woohoo again!", :event  "message", :last-event-id  ""}]
-       (with-test-server
-         (let [[events state close] (client/subscribe (str "http://localhost:" test-server/port "/multiple-events") {})
-               event-vec [(<!! events) (<!! events)]]
-           (close)
-           event-vec)))
+          {:origin  "http://localhost:9890/multiple-events", :data  "woohoo again!", :event  "message", :last-event-id  ""}]
+        (get-events 2 "/multiple-events"))
