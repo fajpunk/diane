e0ef0b95bc107ba2a0ebf71104144a19d75ee322
Dan Fuchs <fuchsd@gmail.com>
2014-08-03 20:54:26 -0500

    Initial speclj conversion

---
+++ test/diane/client_spec.clj
@@ -0,0 +1,86 @@
+(ns diane.client-spec
+  (:require [speclj.core :refer :all]
+            [speclj.run.standard]
+            [diane.client :as client]
+            [clojure.java.io :as io]
+            [clojure.core.async :as async :refer [chan <!! close!]])
+  (:import [java.io StringReader]))
+
+(defn make-stream
+  "Convert the given string into a BufferedInputReader"
+  [string]
+  (io/reader (StringReader. string)))
+
+;; To access private function, the function under test
+(def parse-event-stream #'client/parse-event-stream)
+
+(defn events-for
+  "Returns a vector of all of the events that would be put on a channel for
+  stream represented by the given string"
+  [string & [client-state]] 
+  (let [stream (make-stream string)
+        event-chan (chan 10)
+        client-state (or client-state (atom {:last-event-id ""}))]
+    (parse-event-stream stream event-chan "some-url" client-state)
+    (close! event-chan)
+    (loop [events []
+           value (<!! event-chan)]
+      (if (nil? value)
+        events
+        (recur (conj events value) (<!! event-chan))))))
+
+(describe "Parser"
+          (it "should parse one event" 
+              (should=  [{:origin "some-url", :data "Woohoo!", :event "message", :last-event-id ""}] (events-for "data: Woohoo!\n\n"))
+
+              ;;; An event with no space
+              ;(expect [{:origin "some-url", :data "Woohoo!", :event "message", :last-event-id ""}]
+              ;        (events-for "data:Woohoo!\n\n"))
+
+              ;;; Two events
+              ;(expect [{:origin "some-url", :data "Woohoo!", :event "message", :last-event-id ""}
+              ;         {:origin "some-url", :data "Woohoo again!", :event "message", :last-event-id ""}]
+              ;        (events-for "data: Woohoo!\n\ndata: Woohoo again!\n\n"))
+
+              ;;; Multiline event
+              ;(expect [{:origin "some-url", :data "Woohoo!\nWoohoo more!\nand more!", :event "message", :last-event-id ""}]
+              ;        (events-for "data: Woohoo!\ndata: Woohoo more!\ndata: and more!\n\n"))
+
+              ;;; Named event
+              ;(expect [{:origin "some-url", :data "Woohoo!\nWoohoo more!\nand more!", :event "my-event", :last-event-id ""}]
+              ;        (events-for "event: my-event\ndata: Woohoo!\ndata: Woohoo more!\ndata: and more!\n\n"))
+
+              ;;; Empty event
+              ;(expect [{:origin "some-url", :data "" , :event "my-event", :last-event-id ""}]
+              ;        (events-for "event: my-event\ndata\n\n"))
+
+              ;;; Empty event with colon
+              ;(expect [{:origin "some-url", :data "" , :event "my-event", :last-event-id ""}]
+              ;        (events-for "event: my-event\ndata:\n\n"))
+
+              ;;; A comment
+              ;(expect []
+              ;        (events-for ":a comment"))
+
+              ;;; Events with ids
+              ;(expect [{:origin "some-url", :data "Woohoo!", :event "message", :last-event-id "some-id"}
+              ;         {:origin "some-url", :data "Woohoo again!", :event "message", :last-event-id "another-id"}
+              ;         {:origin "some-url", :data "No id on me.", :event "message", :last-event-id "another-id"}
+              ;         {:origin "some-url", :data "No id for reals!", :event "message", :last-event-id ""}]
+              ;        (events-for "id: some-id\ndata: Woohoo!\n\nid: another-id\ndata: Woohoo again!\n\ndata: No id on me.\n\nid\ndata: No id for reals!\n\n"))
+
+              ;;; Retry
+              ;(let [client-state (atom {:reconnection-time 1000
+              ;                          :last-event-id ""})]
+              ;  (expect [{:origin "some-url", :data "Woohoo!", :event "message", :last-event-id ""}]
+              ;          (events-for "retry: 4000\ndata: Woohoo!\n\n" client-state))
+              ;  (expect 4000 (:reconnection-time @client-state)))
+
+              ;;; Retry with invalid value
+              ;(let [client-state (atom {:reconnection-time 1000
+              ;                          :last-event-id ""})]
+              ;  (expect [{:origin "some-url", :data "Woohoo!", :event "message", :last-event-id ""}]
+              ;          (events-for "retry: 4000blah\ndata: Woohoo!\n\n" client-state))
+              ;  (expect 1000 (:reconnection-time @client-state))))
+              ))
+(run-specs)
---
+++ test/diane/integration_spec.clj
@@ -0,0 +1,34 @@
+(ns diane.integration-spec
+  (:require [speclj.core :refer :all]
+            [diane.client :as client]
+            [test-server :as server]
+            [clojure.core.async :as async :refer [chan <!! close!]]))
+
+;(defmacro with-test-server
+;  "Start a test server and ensure that it is shut down, no matter what"
+;  [& body]
+;  `(do 
+;     (server/start)
+;     (try
+;       (do ~@body)
+;       (finally (server/stop)))))
+
+;(defn get-events
+;  "Subscribe to an event stream at the test server path and return a vector
+;  with n events from the stream"
+;  [n path]
+;  (with-test-server
+;    (let [[events state close] (client/subscribe (str "http://localhost:" test-server/port path) {})
+;          n-results (async/take n events)
+;          all-results (<!! (async/into [] n-results))]
+;      (close)
+;      all-results)))
+
+;;; One event
+;(expect [{:origin  "http://localhost:9890/one-event", :data  "woohoo!", :event  "message", :last-event-id  ""}]
+;        (get-events 1 "/one-event"))
+
+;;; Multiple events
+;(expect [{:origin  "http://localhost:9890/multiple-events", :data  "woohoo!", :event  "message", :last-event-id  ""}
+;         {:origin  "http://localhost:9890/multiple-events", :data  "woohoo again!", :event  "message", :last-event-id  ""}]
+;        (get-events 2 "/multiple-events"))
